562a34122e05106e7c63c969fc5cf3c80cdb917c
tranxuanmanh1611 <manh.manh1611@gmail.com>
2022-10-30 14:14:24 +0700

    bài tập luyện tập cấu trúc điều kiện 01

---
+++ 4.JS/THnhungJS.js
@@ -0,0 +1,13 @@
+function showMessage1() {
+    alert('Xin chào chàng trai trẻ tôi thấy bạn vừa nhấn vào cái nút. ' +
+        'Sao không thử nhấn thêm vào cái nút bên dưới đi nào')
+
+}
+function showMessage2() {
+    alert('tôi biết bạn đang trong giờ thực hành đó :D.' +
+        'Sao không thử nhấn vào cái nút tiếp theo xem')
+
+}
+function showMessage3() {
+alert('Xin chào hoài mệt quá:3')
+}
