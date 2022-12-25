cdd469e72a22ece090344ebe80430faf07a57b34
tranxuanmanh1611 <manh.manh1611@gmail.com>
2022-12-25 14:59:35 +0700

    nopbt

--- module2/4.Lop_va_doi_tuong/bai_tap/lop_stop_watch/src/Main.java
+++ module2/4.Lop_va_doi_tuong/bai_tap/lop_stop_watch/src/Main.java
@@ -1,9 +1,16 @@
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args){
         StopWatch watch1 = new StopWatch();
         watch1.start();
-        for (int i = 0; i < 1000; i++) {
-            System.out.println(i)   ;
+        int[] array = new int[100000];
+        for (int i = 0; i < array.length; i++) {
+            Random rand = new Random();
+            array[i]= rand.nextInt();
+        }
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
         }
       watch1.stop();
         System.out.println("thời gian thuc thi:"+ watch1.getElapsedTime()+" ms");
