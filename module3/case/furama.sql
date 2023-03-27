09a94d43b6c22a1d33a3946ed410baaaaece2f11
tranxuanmanh1611 <manh.manh1611@gmail.com>
2023-03-27 22:22:26 +0700

    task12

--- module3/case/furama.sql
+++ module3/case/furama.sql
@@ -252,9 +252,9 @@ JOIN dich_vu
 ON hop_dong.ma_dich_vu = dich_vu.ma_dich_vu
 JOIN loai_dich_vu 
 ON loai_dich_vu.ma_loai_dich_vu = dich_vu.ma_loai_dich_vu
-WHERE YEAR(hop_dong.ngay_lam_hop_dong) =2020 AND hop_dong.ma_dich_vu NOT IN (SELECT hop_dong.ma_dich_vu 
-																			FROM hop_dong
-																			WHERE YEAR(hop_dong.ngay_lam_hop_dong) = 2021)
+WHERE YEAR(hop_dong.ngay_lam_hop_dong) =2020 
+AND hop_dong.ma_dich_vu NOT IN (SELECT hop_dong.ma_dich_vu FROM hop_dong
+								WHERE YEAR(hop_dong.ngay_lam_hop_dong) = 2021)
 GROUP BY 1;
 
 -- 8.	Hiển thị thông tin ho_ten khách hàng có trong hệ thống, với yêu cầu ho_ten không trùng nhau.
@@ -305,5 +305,19 @@ SELECT hop_dong.ma_hop_dong,
        khach_hang.ho_ten AS ten_khach_hang,
        khach_hang.so_dien_thoai AS sdt_khach,
        dich_vu.ten_dich_vu,
+       SUM(IF(hop_dong_chi_tiet.so_luong IS NULL,0,hop_dong_chi_tiet.so_luong)) AS So_luong_dv_di_kem,
+       hop_dong.tien_dat_coc
+FROM hop_dong
+JOIN dich_vu ON hop_dong.ma_dich_vu = dich_vu.ma_dich_vu
+JOIN nhan_vien ON hop_dong.ma_nhan_vien = nhan_vien.ma_nhan_vien
+JOIN khach_hang ON khach_hang.ma_khach_hang = hop_dong.ma_khach_hang
+LEFT JOIN hop_dong_chi_tiet ON hop_dong.ma_hop_dong =hop_dong_chi_tiet.ma_hop_dong
+WHERE hop_dong.ngay_lam_hop_dong >='2020-10-01' 
+AND hop_dong.ngay_lam_hop_dong<='2020-12-31'
+ AND hop_dong.ma_dich_vu NOT IN (
+SELECT ma_dich_vu FROM hop_dong
+WHERE ngay_lam_hop_dong BETWEEN '2021-01-01' AND '2021-06-30')
+ GROUP BY 1;
+
        
        
